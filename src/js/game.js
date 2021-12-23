fd1a5d5a6f51ae517592e628183890c57891b045
Marilzon <maxmaril@hotmail.com>
2021-12-23 16:22:21 -0300

    animating images of game

--- src/js/game.js
+++ src/js/game.js
@@ -3,10 +3,10 @@ const startContainer = document.querySelector("#start-container");
 function start() {
   $(startContainer).hide();
 
-  $("#game-container").append(`<div id="player"></div>`);
-  $("#game-container").append(`<div id="enemy1"></div>`);
+  $("#game-container").append(`<div class="animationApache" id="player"></div>`);
+  $("#game-container").append(`<div class="animationEnemy1" id="enemy1"></div>`);
   $("#game-container").append(`<div id="enemy2"></div>`);
-  $("#game-container").append(`<div id="friend"></div>`);
+  $("#game-container").append(`<div class="animationFriend" id="friend"></div>`);
 }
 
 startContainer.addEventListener("click", start);
